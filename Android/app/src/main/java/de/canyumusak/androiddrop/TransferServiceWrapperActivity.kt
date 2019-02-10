f34d4bb624e4817c6b072d789a47ecbf40b89d81
Can Yumusak <can.yumusak@quartett-mobile.de>
2019-02-10 16:35:29 +0100

    Finished send_multiple

--- Android/app/src/main/java/de/canyumusak/androiddrop/TransferServiceWrapperActivity.kt
+++ Android/app/src/main/java/de/canyumusak/androiddrop/TransferServiceWrapperActivity.kt
@@ -7,6 +7,22 @@ import android.os.Build
 import android.os.Bundle
 
 class TransferServiceWrapperActivity : Activity() {
+
+    private val dataUris: Array<Uri>?
+        get() {
+            return if (intent.action == Intent.ACTION_SEND) {
+                (intent.extras?.get(TransferService.DATA) as Uri?)?.let {
+                    arrayOf(it)
+                }
+            } else if (intent.action == Intent.ACTION_SEND_MULTIPLE) {
+                (intent.extras?.get(TransferService.DATA) as List<Uri?>?)?.let {
+                    it.filterNotNull().toTypedArray()
+                }
+            } else {
+                null
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,12 +30,10 @@ class TransferServiceWrapperActivity : Activity() {
         serviceIntent.putExtra(TransferService.CLIENT_NAME, intent.extras[TransferService.CLIENT_NAME] as String)
         serviceIntent.putExtra(TransferService.IP_ADDRESS, intent.extras[TransferService.IP_ADDRESS] as String)
         serviceIntent.putExtra(TransferService.PORT, intent.extras[TransferService.PORT] as Int )
-
-        TODO()
-
-        val dataUri = intent?.extras?.get(TransferService.DATA) as Uri
-        serviceIntent.putExtra(TransferService.DATA, dataUri)
-        grantUriPermission(packageName, dataUri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        serviceIntent.putExtra(TransferService.DATA, dataUris)
+        dataUris?.forEach {
+            grantUriPermission(packageName, it, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             startForegroundService(serviceIntent)
