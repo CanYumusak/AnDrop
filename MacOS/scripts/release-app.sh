97c47f84b7b89fb04f48b015dde38e455f621e89
Can Yumusak <can.yumusak@quartett-mobile.de>
2019-02-11 21:21:27 +0100

    Fixed release script

--- MacOS/scripts/release-app.sh
+++ MacOS/scripts/release-app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm -rf "release/*"
+mkdir "release"
+rm -rf "build/*"
 
 xcodebuild \
 	-scheme "AndroidDrop" \
@@ -24,8 +26,8 @@ create-dmg \
 	--window-pos 200 120 \
 	--window-size 800 400 \
 	--icon-size 100 \
-	--icon "Application.app" 200 190 \
-	--hide-extension "Application.app" \
+	--icon "AndroidDrop.app" 200 190 \
+	--hide-extension "AndroidDrop.app" \
 	--app-drop-link 600 185 \
 	"release/Androp-Installer.dmg" \
 	"build/release/"
