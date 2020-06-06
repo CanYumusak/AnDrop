b02979c8787f64926bc3f6fa93c65fb840c8c27d
Can Yumusak <can.yumusak@quartett-mobile.de>
2020-06-06 16:46:38 +0200

    small refactorings left and right

--- Android/app/src/main/java/de/canyumusak/androiddrop/TransferServiceWrapperActivity.kt
+++ Android/app/src/main/java/de/canyumusak/androiddrop/TransferServiceWrapperActivity.kt
@@ -27,9 +27,9 @@ class TransferServiceWrapperActivity : Activity() {
         super.onCreate(savedInstanceState)
 
         val serviceIntent = Intent(this, TransferService::class.java)
-        serviceIntent.putExtra(TransferService.CLIENT_NAME, intent.extras[TransferService.CLIENT_NAME] as String)
-        serviceIntent.putExtra(TransferService.IP_ADDRESS, intent.extras[TransferService.IP_ADDRESS] as String?)
-        serviceIntent.putExtra(TransferService.PORT, intent.extras[TransferService.PORT] as Int )
+        serviceIntent.putExtra(TransferService.CLIENT_NAME, intent.extras!![TransferService.CLIENT_NAME] as String)
+        serviceIntent.putExtra(TransferService.IP_ADDRESS, intent.extras!![TransferService.IP_ADDRESS] as String?)
+        serviceIntent.putExtra(TransferService.PORT, intent.extras!![TransferService.PORT] as Int)
         serviceIntent.putExtra(TransferService.DATA, dataUris)
         dataUris?.forEach {
             grantUriPermission(packageName, it, Intent.FLAG_GRANT_READ_URI_PERMISSION)
