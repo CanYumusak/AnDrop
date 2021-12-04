581b0c4add9718866210f9b85b91fad5bfcbaf2b
Can Yumusak <can.yumusak@quartett-mobile.de>
2021-12-04 18:34:31 +0100

    Raised versions

--- Android/build.gradle.kts
+++ Android/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application") version "7.2.0-alpha05" apply false
     id("com.android.library") version "7.2.0-alpha05" apply false
-    kotlin("android") version "1.5.31" apply false
-    kotlin("plugin.serialization") version "1.5.31"
+    kotlin("android") version "1.6.0" apply false
+    kotlin("plugin.serialization") version "1.6.0"
 }
 
 tasks.register("clean", Delete::class) {
