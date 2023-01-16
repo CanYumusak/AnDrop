5998376381db6476236d91b1478cb8783b1168eb
Can Yumusak <cyumusak@squareup.com>
2023-01-16 21:42:48 +0100

    Raised dependency versions

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 import java.io.FileInputStream
 import java.util.*
 
@@ -7,7 +9,7 @@ plugins {
     kotlin("plugin.serialization")
 }
 
-val composeVersion = "1.1.0-beta04"
+val composeVersion = "1.3.2"
 
 android {
     buildFeatures {
@@ -15,13 +17,13 @@ android {
         compose = true
     }
 
-    compileSdk = 31
+    compileSdk = 33
     defaultConfig {
         applicationId = "de.canyumusak.androiddrop"
         minSdk = 24
-        targetSdk = 31
-        versionCode = 155
-        versionName = "1.5.5"
+        targetSdk = 33
+        versionCode = 156
+        versionName = "1.6.0"
     }
 
     signingConfigs {
@@ -53,42 +55,42 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_15
-        targetCompatibility = JavaVersion.VERSION_15
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     composeOptions {
         kotlinCompilerExtensionVersion = composeVersion
     }
-
     kotlinOptions {
-        jvmTarget = "15"
+        jvmTarget = "1.8"
     }
+    namespace = "de.canyumusak.androiddrop"
 }
 
 dependencies {
-    implementation("androidx.compose.material3:material3:1.0.0-alpha02")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha04")
 
-    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.activity:activity-compose:1.6.1")
 
     implementation("androidx.compose.ui:ui:${composeVersion}")
     implementation("androidx.compose.ui:ui-tooling-preview:${composeVersion}")
-    implementation("androidx.compose.material:material:${composeVersion}")
+    implementation("androidx.compose.material:material:1.3.1")
     debugImplementation("androidx.compose.ui:ui-tooling:${composeVersion}")
     debugImplementation("androidx.compose.ui:ui-test-manifest:${composeVersion}")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
-    implementation("androidx.fragment:fragment-ktx:1.4.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.fragment:fragment-ktx:1.5.5")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
-    implementation("com.android.billingclient:billing:4.0.0")
+    implementation("com.android.billingclient:billing:5.1.0")
 }
--- Android/build.gradle.kts
+++ Android/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application") version "7.2.2" apply false
-    id("com.android.library") version "7.2.2" apply false
-    kotlin("android") version "1.6.0" apply false
-    kotlin("plugin.serialization") version "1.6.0"
+    id("com.android.application") version "7.4.0" apply false
+    id("com.android.library") version "7.4.0" apply false
+    kotlin("android") version "1.7.20" apply false
+    kotlin("plugin.serialization") version "1.7.20"
 }
 
 tasks.register("clean", Delete::class) {
