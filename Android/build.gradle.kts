c3191e6406e673897f1ab3077f1bb50304863078
Can Yumusak <cyumusak@squareup.com>
2023-01-16 21:25:14 +0100

    Raised Gradle, AGP

--- Android/build.gradle.kts
+++ Android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.2.0-alpha05" apply false
-    id("com.android.library") version "7.2.0-alpha05" apply false
+    id("com.android.application") version "7.2.2" apply false
+    id("com.android.library") version "7.2.2" apply false
     kotlin("android") version "1.6.0" apply false
     kotlin("plugin.serialization") version "1.6.0"
 }
