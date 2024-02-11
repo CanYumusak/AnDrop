b203d2a8889a7f46fdb4a37cedeb2080761dbdee
Can Yumusak <cyumusak@squareup.com>
2024-02-11 22:49:36 +0100

    Added Creation Date support for ContentProvide file (#17)
    * Added translations
    
    * Raised java version
    
    * Added Content Provider Creation Date support

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "de.canyumusak.androiddrop"
         minSdk = 26
         targetSdk = 33
-        versionCode = 207
-        versionName = "2.0.7"
+        versionCode = 208
+        versionName = "2.0.8"
     }
 
     signingConfigs {
