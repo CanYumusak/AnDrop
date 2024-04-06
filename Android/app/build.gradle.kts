817df8b18ba93662c93b1b1e939f33925be0d634
Can Yumusak <cyumusak@squareup.com>
2024-04-06 12:46:53 +0200

    Can/feature/inapp review (#19)
    * Implemented In-App Review
    
    * Raised version

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "de.canyumusak.androiddrop"
         minSdk = 26
         targetSdk = 33
-        versionCode = 210
-        versionName = "2.1.0"
+        versionCode = 211
+        versionName = "2.1.1"
     }
 
     signingConfigs {
@@ -89,6 +89,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
     implementation("androidx.activity:activity-compose")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("com.google.android.play:review-ktx:2.0.1")
 
     implementation("app.rive:rive-android:4.0.0")
     implementation("androidx.startup:startup-runtime:1.1.1")
